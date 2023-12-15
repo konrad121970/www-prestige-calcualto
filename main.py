eeab2dbc5011eb26dc914f6381ee6b05c9d3f798
konrad121970 <konrad121970@windowslive.com>
2023-12-15 01:20:59 +0100

    Follow Python's naming conventions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from IncidenceListReader import IncidenceListReader
+from incidence_list_reader import IncidenceListReader
 
 
-incidence_list_path = 'incidence_list.txt'
+incidence_list_path = 'resources/incidence_list.txt'
 
 incidence_list_reader = IncidenceListReader(incidence_list_path)
 incidence_list_reader.read_incidents_from_file()
