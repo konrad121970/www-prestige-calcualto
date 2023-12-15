43cf469471c425fc3d26ac954e2bbda8693db19f
konrad121970 <konrad121970@windowslive.com>
2023-12-15 01:24:33 +0100

    Add comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from incidence_list_reader import IncidenceListReader
 
-
 incidence_list_path = 'resources/incidence_list.txt'
 
 incidence_list_reader = IncidenceListReader(incidence_list_path)
@@ -9,3 +8,5 @@ incidence_list_reader.read_incidents_from_file()
 incidence_list = incidence_list_reader.get_incidents()
 
 print(incidence_list)
+
+# Output: [('strona A', ['strona B', 'strona C']), ('strona B', ['strona C']), ('strona C', [''])]
